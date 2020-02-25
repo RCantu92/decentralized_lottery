b07d9e5860241bf0edbbc181f61bfe5796ba6975
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-02-25 16:03:37 +0900

    Created test.js file for the lottery contract.

--- compile.js
+++ compile.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const solc = require('solc');
 
 // Creating a variable to set the file path to the
-// inbox directory from the home directory.
+// lottery directory from the home directory.
 // Then telling it to go to the 'contracts' folder then
-// read the inbox solidity file.
-const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+// read the lottery solidity file.
+const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol');
 // Creating a variable to read the contents of a file.
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-// Export the 'inbox' contents of the solidity compiler output
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+// Export the 'lottery' contents of the solidity compiler output
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
 // console.log(solc.compile(source, 1))
