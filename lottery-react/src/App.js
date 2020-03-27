44be460402217a2ecb9a3f540cf440ca66ea3c67
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-03-27 16:35:44 -0500

    Working on reactjs component and rendering

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3.js';
+import lottery from './lottery.js';
+ 
+class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  };
 
-function App() {
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {this.state.manager}.
+        There are currently {this.state.players.length} people entered,
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+      </p>
     </div>
   );
-}
-
+}}
+ 
 export default App;
