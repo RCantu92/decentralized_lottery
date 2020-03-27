9a4c55070ea4820bd95a043de11b77f243fb0ee9
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-03-27 17:14:14 -0500

    Completed decentralized lottery project with a basic react front end.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -34,7 +34,19 @@ class App extends Component {
     });
 
     this.setState({ message: 'You have been entered!' });
-  };
+  }
+
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
+  }
 
   render(){
   return (
@@ -59,6 +71,11 @@ class App extends Component {
         </div>
         <button>Enter</button>
       </form>
+
+      <hr />
+
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={this.onClick}>Pick a winner!</button>
 
       <hr />
 
