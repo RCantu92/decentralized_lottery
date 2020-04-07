9c7c9ac01e1b217612c07fe0e65cc1b6dbb41bc8
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-04-07 17:41:58 -0500

    Updated comments on lottery.test.js

--- README.md
+++ README.md
@@ -26,10 +26,17 @@ This contract is deployed on the rinkeby testnet under address [0x40102DAa67EE7c
 
 ## Built With
 
+* [npm](https://www.npmjs.com/) - Package manager for the JavaScript programming language.
 * [Solidity, v.4.17.0](https://solidity.readthedocs.io/en/v0.4.17/) - Smart Contract programming language used.
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/javascript) - Programming language used.
 * [Path](https://nodejs.org/api/path.html) - Module that provides ability for working with file and directory paths.
 * [File System](https://nodejs.org/api/fs.html) - Module that provides an API for interacting with the file system.
 * [solc](https://github.com/ethereum/solc-js) - Module for compiling the Solidity programming language.
+* [Mocha](https://mochajs.org/) - JavaScript test framework.
+* [Ganache CLI](https://www.npmjs.com/package/ganache-cli) - the command line version of Ganache, your personal blockchain for Ethereum development.
+* [web3](https://github.com/ethereum/web3.js/) - This is the Ethereum JavaScript API which connects to the Generic JSON-RPC spec. Used v.1.0.0-beta.35.
+* [Assert](https://nodejs.org/api/assert.html) - Module that provides a set of assertion functions for verifying invariants.
+
 * [Infura](https://infura.io/) - API used to access the Ethereum and IPFS networks.
 * [truffle-hdwallet-provider](https://github.com/trufflesuite/truffle-hdwallet-provider) - HD Wallet-enabled Web3 provider. Used to sign transactions for addresses derived from a 12-word mnemonic.
 * [Create React App](https://github.com/facebook/create-react-app) - Tool to build React apps.
--- lottery-smart-contract/test/lottery.test.js
+++ lottery-smart-contract/test/lottery.test.js
@@ -1,26 +1,50 @@
+// Load in assert module
 const assert = require('assert');
+// Load in Ganache CLI
 const ganache = require('ganache-cli');
+// Load in web3
 const Web3 = require('web3');
+// Create an instance of web3 that connects
+// to the ganache local blockchain 
+// and saves it to a variable.
 const web3 = new Web3(ganache.provider());
-
+// Import interface and bytecode from compile.js
 const { interface, bytecode } = require('../compile');
 
+// Declaring two variables with no values.
 let lottery;
 let accounts;
 
+/**
+ * @dev Getting the accounts and deploying our
+ * contract at the beginning of every test.
+ */
 beforeEach(async () => {
+    // Get a list of generated accounts and
+    // save to previously declared variable.
     accounts = await web3.eth.getAccounts();
 
+    /**
+     * @dev Saving the instance of the deployed contract.
+     * @param interface ABI from compiled contract.
+     * Used JSON.parse because Contract() requires JavaScript object.
+     * @param data contract's bytecode.
+     * @param gas fee paid to the network to run code. Counted in wei.
+     * @param from account from which gas is being paid from.
+     */
     lottery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Lottery Contract', () => {
+    // Verifies our contract was deployed.
     it('deploys a contract', () => {
         assert.ok(lottery.options.address);
     });
 
+    // Verifies that the players address was
+    // successfully entered into the lottery.
     it('allows one account to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
@@ -35,6 +59,8 @@ describe('Lottery Contract', () => {
         assert.equal(1, players.length);
     });
 
+    // Verifies the contract allows multiples players
+    // to enter the lottery.
     it('allows multiple accounts to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
